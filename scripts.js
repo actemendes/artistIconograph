28a112a48c2a6ed68c001fb3c94cf50d434c17ef
actemendes <kuv2606@gmail.com>
2024-06-28 00:02:01 +0300

    lazy load + local img

--- scripts.js
+++ scripts.js
@@ -56,3 +56,30 @@ for (let smoothLink of smoothLinks) {
         });
     });
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+    const lazyImages = document.querySelectorAll('img.lazy');
+
+    const lazyLoad = (target) => {
+        const options = {
+            root: null,
+            rootMargin: "0px",
+            threshold: 0.1
+        };
+
+        const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    const img = entry.target;
+                    img.src = img.dataset.src;
+                    img.classList.remove('lazy');
+                    observer.unobserve(img);
+                }
+            });
+        }, options);
+
+        observer.observe(target);
+    };
+
+    lazyImages.forEach(lazyLoad);
+});
